fbd493a57bac6249faec32cc55c79c47922fbdf2
Marshall Sykes <rmsykes91@gmail.com>
2019-11-11 12:29:06 -0500

    needed to add nav to homepage, now added

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -24,19 +24,26 @@ export default class HomePage extends Component {
     render() {
         return (
             <div>
-                {/* Accessing the value of message from the state object */}
+
+                <nav>
+                    <Link to='/'>Home</Link>
+                    <Link to='/concert'>Concerts</Link>
+                    <Link to='/band'>Bands</Link>
+                    <Link to='/venue'>Venues</Link>
+                </nav>
+
                 <h1>Home Page</h1>
 
                 <div>
-                    <Link to={'/concert'} >My Concerts<br/><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.goodfreephotos.com%2Falbums%2Fpeople%2Fcrowd-and-lights-at-a-concert.jpg&f=1&nofb=1" alt="concert photo"/></Link>
-                    <br/>
-                    
-                    <Link to={'/band'}>My Bands <br/><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fquality%3D100%26image_uri%3Dhttp%253A%252F%252Fwww.blogcdn.com%252Fwww.joystiq.com%252Fmedia%252F2011%252F08%252Frhcp530pxdph.jpg%26client%3Dcbc79c14efcebee57402%26signature%3Dd5d93a072070c14f57f453016e0340837cd7cc53&f=1&nofb=1" alt="band photo"/></Link>
-                    <br/>
-                    
-                    <Link to={'/venue'}>My Venues <br/><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.charlottefive.com%2Fwp-content%2Fuploads%2F2015%2F01%2Fmusic-venues-visulite-charlotte2.jpg&f=1&nofb=1" alt="venue photo"/></Link>
-                   
-                    
+                    <Link to={'/concert'} >My Concerts<br /><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.goodfreephotos.com%2Falbums%2Fpeople%2Fcrowd-and-lights-at-a-concert.jpg&f=1&nofb=1" alt="concert photo" /></Link>
+                    <br />
+
+                    <Link to={'/band'}>My Bands <br /><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fquality%3D100%26image_uri%3Dhttp%253A%252F%252Fwww.blogcdn.com%252Fwww.joystiq.com%252Fmedia%252F2011%252F08%252Frhcp530pxdph.jpg%26client%3Dcbc79c14efcebee57402%26signature%3Dd5d93a072070c14f57f453016e0340837cd7cc53&f=1&nofb=1" alt="band photo" /></Link>
+                    <br />
+
+                    <Link to={'/venue'}>My Venues <br /><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.charlottefive.com%2Fwp-content%2Fuploads%2F2015%2F01%2Fmusic-venues-visulite-charlotte2.jpg&f=1&nofb=1" alt="venue photo" /></Link>
+
+
                 </div>
 
 
